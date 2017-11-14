b05b2336520f22106b475f225220ce27e4b23123
nietup <22.jakub@gmail.com>
2017-11-14 09:51:15 +0100

    broadcaster

---
+++ Broadcaster/README.md
@@ -0,0 +1,10 @@
+# BroadcastReceiver
+School assignment: android broadcast receiver and notifications 
+
+##App 1
+
+Enables user to compose a message and to broadcast an intent containing that message.
+
+##App 2
+
+Contains a service with broadcast receiver listening for the sent message and creating a notification with it. After clicking the notification, app 1 activity opens.
---
+++ Broadcaster/app/src/main/java/com/example/nietup/broadcaster/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.nietup.broadcaster;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private EditText etMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etMessage = (EditText) findViewById(R.id.am_et_message);
+    }
+
+    /**
+     * Broadcasts notification message so that receiver app can create notification out of it.
+     */
+    public void broadcast(View v) {
+        String msg = etMessage.getText().toString();
+
+        Intent intent = new Intent();
+        intent.setAction("com.example.nietup.broadcaster.VERY_IMPORTANT_MESSAGE");
+        intent.putExtra("data",msg);
+        sendBroadcast(intent);
+
+        Toast.makeText(this, "Message broadcasted", Toast.LENGTH_SHORT).show();
+    }
+}
